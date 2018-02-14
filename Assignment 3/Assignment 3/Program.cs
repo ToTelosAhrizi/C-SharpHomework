000bed1fab027916ad975a77c50ad60383ea2d4e
ToTelosAhrizi <harperdoug.j@gmail.com>
2018-02-14 14:35:50 -0500

    Add files via upload

---
+++ Assignment 3/Assignment 3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            //Execise 2 Assignment 3
+            WriteLine("Execise 2 Assignment 3\n");
+            string x = ("*******************************************************************************************");
+            string y = ("LCC is Lansing community college. It is in Lansing. The school colors are blue and silver.");
+            string meter;
+            string address;
+            string lvalue;
+            double exempt;
+            double mr;
+            double nvaftermr;
+            double lv;
+            double nv;
+            double m;
+            double f;
+            double feet;
+            double i;
+            double inch;
+            WriteLine(a.Sep(x));
+            WriteLine(a.Info(y));
+            WriteLine(a.Sep(x));
+            ReadKey();
+            //Execise 6 Assignment 3
+            WriteLine("\nExecise 6 Assignment 3\n");
+            WriteLine("Enter number of meters");
+            meter = ReadLine();
+            m=double.Parse(meter);
+            f = 3.2808399;
+            i = 39.3700787;
+            feet = m * f;
+            inch = m * i;
+            WriteLine("There are "+f+" feet in one meter so to calculate do "+m+"*"+f);
+            WriteLine("There are " + i + " inches in one meter so to calculate do " + m + "*" + i);
+            WriteLine(m+" meters is "+feet+" feet and  "+inch+" inches");
+            ReadKey();
+            //Execise 10 Assignment 3
+            WriteLine("\nExecise 10 Assignment 3\n");
+            WriteLine("Enter address:");
+            address = ReadLine();
+            WriteLine("\nEnter last years assessed value (above $25,000):");
+            lvalue = ReadLine();
+            lv = double.Parse(lvalue);
+            nv = lv - 25000;
+            mr = .01003;
+            nvaftermr = nv * mr;
+            WriteLine("Your property tax is:");
+            WriteLine("{0:c}",nvaftermr);
+
+           ReadKey();
+        }
+        //Method 1 for execise 2
+        public string Sep(string x)
+        {
+            return x;
+        }
+        //Method 2 for execise 2
+        public string Info(string y)
+        {
+            return y;
+        }
+    }
+}
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Programming exersise two*/
+            WriteLine("Program 1 assignment 2\n");
+            double miles = 5;
+            double feet = miles * 5280;
+            double km = miles * 1.6;
+            WriteLine("Number of miles:");
+            WriteLine("{0:####.##}", miles);
+            WriteLine("Number of feet in set miles:");
+            WriteLine("{0:#####.##}", feet);
+            WriteLine("Number of kilometers in set miles:");
+            WriteLine("{0:####.##}", km);
+            ReadKey();
+            /*Programming exersise 3*/
+            WriteLine("\nProgram 2 assignment 2\n");
+            double bill = 20;
+            double fiftip = bill * 0.15;
+            double twentip = bill * 0.2;
+            double fifbill = bill + fiftip;
+            double twenbill = bill + twentip;
+            WriteLine("Your bill is:");
+            WriteLine("{0:c}",bill);
+            WriteLine("A fifteen percent tip is:");
+            WriteLine("{0:c}", fiftip);
+            WriteLine("A twenty percent tip is:");
+            WriteLine("{0:c}", twentip);
+            WriteLine("Bill with 15% tip");
+            WriteLine("{0:c}", fifbill);
+            WriteLine("Bill with 20% tip"); 
+            WriteLine("{0:c}", twenbill);
+            ReadKey();
+            /*Programming exersise ten
+             1 pound = 453.59237 grams*/
+            WriteLine("\nProgram 3 assignment 2\n");
+            double gramspout = 100;
+            double gramshv = 100;
+            double poundspout = gramspout / 453.59237;
+            double poundshv = gramshv / 453.59237;
+            WriteLine("100 grams of poutine in pounds " + poundspout);
+            WriteLine("100 grams of haricot in pounds " + poundshv);
+
+           
+            ReadKey();
+        }
+    }
+}
